56316e997f3560a4ed7ed5953eb4e3be54481ab9
David Samuelson <linksn.1fan@gmail.com>
2022-06-13 11:26:00 -0600

    added login

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,11 +4,7 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-    signToken: function({ username, email, _id}) {
-        const payload = {username, email, _id };
-
-        return jwt.sign({data: payload}, secret, {expiresIn: expiration });
-    },
+    
     authMiddleware: function({req}) {
         let token = req.body.token || req.query.token || req.headers.authorization;
 
@@ -31,5 +27,10 @@ module.exports = {
         }
 
         return req;
+    },
+    signToken: function({ username, email, _id}) {
+        const payload = {username, email, _id };
+
+        return jwt.sign({data: payload}, secret, {expiresIn: expiration });
     }
 };

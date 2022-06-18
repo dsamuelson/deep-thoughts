8faf2e6eedfe5b825d185577f72511eeabef5c5e
David Samuelson <linksn.1fan@gmail.com>
2022-06-17 18:13:12 -0600

    fixed profile lookup for me in server

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,14 +6,18 @@ import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 import Auth from '../utils/auth';
 
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+
+import { ADD_FRIEND } from '../utils/mutations'
 
 const Profile = (props) => {
   const { username: userParam } = useParams();
-  const { loading, data } = useQuery(QUERY_USER, {
-    variables: { username: userParam || Auth.getProfile().data.username },
+  const { loading, data } = useQuery(userParam? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam},
   });
+
+  const [addFriend] = useMutation(ADD_FRIEND);
   
   const user = data?.me || data?.user || {};
 
@@ -34,12 +38,25 @@ const Profile = (props) => {
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+        {Auth.getProfile().data.username !== user.username ? (
+          <button className='btn ml-auto' onClick={handleClick}> Add Friend </button>
+        ) : null }
       </div>
 
       <div className="flex-row justify-space-between mb-3">
